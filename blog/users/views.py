d3cf5c43264041bbee39e48256f67cbb903a741d
yjwei21 <wei19870118@126.com>
2021-06-05 17:28:14 +0800

    '定义用户模型类’

---
+++ blog/users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# 注册视图
+from django.views import View
+
+
+class RegisterView(View):
+
+    def get(self, request):
+
+        return render(request, 'register.html')
